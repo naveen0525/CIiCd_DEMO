dca37c6ff771297fa8c03a5a6769ed6f2ed3b405
naveen0525 <149082605+naveen0525@users.noreply.github.com>
2023-11-17 21:31:56 +0530

    Update SeleniumBaseClass.java

--- src/test/java/Utilities/SeleniumBaseClass.java
+++ src/test/java/Utilities/SeleniumBaseClass.java
@@ -15,29 +15,14 @@ public class SeleniumBaseClass {
     public WebDriver setUp() {
         Properties prop = readPropertiesFile(".\\resources\\Property\\essential.properties");
 
-//        System.setProperty("webdriver.edge.driver", ".\\resources\\drivers\\edge\\msedgedriver.exe");
-
-     /*   EdgeOptions options = new EdgeOptions();
-        // ChromeDriver is just AWFUL because every version or two it breaks unless you pass cryptic arguments
-        //AGRESSIVE: options.setPageLoadStrategy(PageLoadStrategy.NONE); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
-        options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
-        options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
-        options.addArguments("--headless"); // only if you are ACTUALLY running headless
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
-        options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
-        options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
-*/
-        // Set up the wWebDriverManager for chrome driver
-        WebDriverManager.chromedriver().setup();
-        // Create the driver object
-
-        driver = new ChromeDriver();
-
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get(prop.getProperty("url"));
-        driver.manage().window().maximize();
+//        WebDriverManager.chromedriver().setup();
+	    ChromeOptions options = new ChromeOptions();
+	    options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+	    driver = new ChromeDriver(options);
+	    driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         return driver;
     }
