eddf2a6d02593fa39f95718aaafdadadbfa861b6
naveen0525 <149082605+naveen0525@users.noreply.github.com>
2023-11-20 19:55:22 +0530

    Runs at 7.55PM on 20112023

--- src/test/java/TestClasses/LoginTest.java
+++ src/test/java/TestClasses/LoginTest.java
@@ -34,6 +34,7 @@ public class LoginTest extends SeleniumBaseClass {
         htmlReporter.config().setReportName("Test Report");
         htmlReporter.config().setTheme(Theme.STANDARD);
         htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+        
     }
 
     @Test
